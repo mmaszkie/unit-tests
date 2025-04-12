1e0211d84988d92f3693345f3847fbc0ff013d0e
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 15:09:35 +0200

    CitiesSearch empty implementation

--- src/main/java/com/example/unittests/task2/CitiesSearch.java
+++ src/main/java/com/example/unittests/task2/CitiesSearch.java
@@ -1,5 +1,30 @@
 package com.example.unittests.task2;
 
+import java.util.Set;
+
 class CitiesSearch {
+
+    private static final Set<String> AVAILABLE_CITIES = Set.of(
+            "Paris",
+            "Budapest",
+            "Skopje",
+            "Rotterdam",
+            "Valencia",
+            "Vancouver",
+            "Amsterdam",
+            "Vienna",
+            "Sydney",
+            "New York City",
+            "London",
+            "Bangkok",
+            "Hong Kong",
+            "Dubai",
+            "Rome",
+            "Istanbul"
+    );
+
+    Set<String> search(String inputText) {
+        throw new RuntimeException("Not implemented yet");
+    }
 
 }
