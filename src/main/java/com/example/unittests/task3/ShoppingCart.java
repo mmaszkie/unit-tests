dfaf0564346ba2a3e0beedc8493959dfdcc548fd
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 17:36:07 +0200

    Add shopping cart dtos

--- src/main/java/com/example/unittests/task3/ShoppingCart.java
+++ src/main/java/com/example/unittests/task3/ShoppingCart.java
@@ -1,5 +1,14 @@
 package com.example.unittests.task3;
 
+import java.math.BigDecimal;
+
 class ShoppingCart {
+
+    void addItem(CartItem item) {
+    }
+
+    BigDecimal getTotalPrice() {
+        throw new RuntimeException("Not implemented yet");
+    }
 
 }
