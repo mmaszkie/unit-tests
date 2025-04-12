9449a361172a6a6afc58e3132ea44b2680926f62
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 14:43:58 +0200

    Add implementation to some test cases

--- src/test/java/com/example/unittests/task1/DiscountCalculatorTests.java
+++ src/test/java/com/example/unittests/task1/DiscountCalculatorTests.java
@@ -1,12 +1,28 @@
 package com.example.unittests.task1;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DiscountCalculatorTests {
 
+    private final DiscountCalculator discountCalculator = new SimpleDiscountCalculator();
+
     @Test
     public void shouldThrowExceptionWhenAmountIsNull() {
+        // given
+        BigDecimal amount = null;
 
+        // when
+        Executable actualDiscount = () -> discountCalculator.calculate(amount);
+
+        // then
+        RuntimeException actualException = assertThrows(RuntimeException.class, actualDiscount);
+        assertEquals("Amount value must be greater than or equal to zero", actualException.getMessage());
     }
 
     @Test
