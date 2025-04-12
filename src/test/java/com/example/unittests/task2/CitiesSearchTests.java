ea9d4ee21279a384545dfa648da93853938b9953
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 15:12:13 +0200

    Add some tests

--- src/test/java/com/example/unittests/task2/CitiesSearchTests.java
+++ src/test/java/com/example/unittests/task2/CitiesSearchTests.java
@@ -1,5 +1,38 @@
 package com.example.unittests.task2;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static java.util.Collections.EMPTY_SET;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CitiesSearchTests {
+
+    private final CitiesSearch citiesSearch = new CitiesSearch();
+
+    @Test
+    public void shouldNotFindAnyCityWhenInputTextIsNull() {
+        // given
+        String inputText = null;
+
+        // when
+        Set<String> foundCities = citiesSearch.search(inputText);
+
+        // then
+        assertEquals(EMPTY_SET, foundCities);
+    }
+
+    @Test
+    public void shouldNotFindAnyCityWhenInputTextIsTooShort() {
+        // given
+        String inputText = "B";
+
+        // when
+        Set<String> foundCities = citiesSearch.search(inputText);
+
+        // then
+        assertEquals(EMPTY_SET, foundCities);
+    }
 
 }
