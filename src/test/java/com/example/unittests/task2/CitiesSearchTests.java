ea4a2e0b52940ac7949eab99c710712e6877455d
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 15:31:09 +0200

    Introduce parametrized tests

--- src/test/java/com/example/unittests/task2/CitiesSearchTests.java
+++ src/test/java/com/example/unittests/task2/CitiesSearchTests.java
@@ -1,116 +1,85 @@
 package com.example.unittests.task2;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static java.util.Collections.EMPTY_SET;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.argumentSet;
 
 class CitiesSearchTests {
 
     private final CitiesSearch citiesSearch = new CitiesSearch();
 
-    @Test
-    public void shouldNotFindAnyCityWhenInputTextIsNull() {
-        // given
-        String inputText = null;
-
-        // when
-        Set<String> foundCities = citiesSearch.search(inputText);
-
-        // then
-        assertEquals(EMPTY_SET, foundCities);
-    }
-
-    @Test
-    public void shouldNotFindAnyCityWhenInputTextIsTooShort() {
-        // given
-        String inputText = "B";
-
-        // when
-        Set<String> foundCities = citiesSearch.search(inputText);
-
-        // then
-        assertEquals(EMPTY_SET, foundCities);
-    }
-
-    @Test
-    public void shouldFindCitiesThatStartWithInputText() {
-        // given
-        String inputText = "Va";
-
-        // when
-        Set<String> foundCities = citiesSearch.search(inputText);
-
-        // then
-        assertEquals(Set.of("Vancouver", "Valencia"), foundCities);
-    }
-
-    @Test
-    public void shouldFindCitiesThatContainInputText() {
-        // given
-        String inputText = "ape";
-
-        // when
-        Set<String> foundCities = citiesSearch.search(inputText);
-
-        // then
-        assertEquals(Set.of("Budapest"), foundCities);
-    }
-
-    @Test
-    public void shouldFindCitiesThatEndWithInputText() {
-        // given
-        String inputText = "rdam";
-
-        // when
-        Set<String> foundCities = citiesSearch.search(inputText);
-
-        // then
-        assertEquals(Set.of("Rotterdam", "Amsterdam"), foundCities);
-    }
-
-    @Test
-    public void shouldFindCitiesIgnoringCaseDifferences() {
-        // given
-        String inputText = "vA";
-
-        // when
-        Set<String> foundCities = citiesSearch.search(inputText);
-
-        // then
-        assertEquals(Set.of("Vancouver", "Valencia"), foundCities);
-    }
-
-    @Test
-    public void shouldFindAllCitiesWhenInputTextIsAsterisk() {
-        // given
-        String inputText = "*";
-        Set<String> expectedCities = Set.of(
-                "Paris",
-                "Budapest",
-                "Skopje",
-                "Rotterdam",
-                "Valencia",
-                "Vancouver",
-                "Amsterdam",
-                "Vienna",
-                "Sydney",
-                "New York City",
-                "London",
-                "Bangkok",
-                "Hong Kong",
-                "Dubai",
-                "Rome",
-                "Istanbul"
-        );
-
-        // when
+    @ParameterizedTest
+    @MethodSource("citiesSearchTestCases")
+    public void shouldPerformCitiesSearch(String inputText, Set<String> expectedCities) {
+        // given / when
         Set<String> foundCities = citiesSearch.search(inputText);
 
         // then
         assertEquals(expectedCities, foundCities);
+    }
+
+    private static Stream<Arguments> citiesSearchTestCases() {
+        return Stream.of(
+                argumentSet(
+                        "should not find any city when input text is null",
+                        null,
+                        EMPTY_SET
+                ),
+                argumentSet(
+                        "should not find any city when input text is too short",
+                        "B",
+                        EMPTY_SET
+                ),
+                argumentSet(
+                        "should find cities that start with input text",
+                        "Va",
+                        Set.of("Vancouver", "Valencia")
+                ),
+                argumentSet(
+                        "should find cities that contain input text",
+                        "ape",
+                        Set.of("Budapest")
+                ),
+                argumentSet(
+                        "should find cities that end with with input text",
+                        "rdam",
+                        Set.of("Rotterdam", "Amsterdam")
+                ),
+                argumentSet(
+                        "should find cities ignoring case differences",
+                        "vA",
+                        Set.of("Vancouver", "Valencia")
+                ),
+                argumentSet(
+                        "should find all cities when input text is asterisk",
+                        "*",
+                        Set.of(
+                                "Paris",
+                                "Budapest",
+                                "Skopje",
+                                "Rotterdam",
+                                "Valencia",
+                                "Vancouver",
+                                "Amsterdam",
+                                "Vienna",
+                                "Sydney",
+                                "New York City",
+                                "London",
+                                "Bangkok",
+                                "Hong Kong",
+                                "Dubai",
+                                "Rome",
+                                "Istanbul"
+                        )
+                )
+        );
     }
 
 }
