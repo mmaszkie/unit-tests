ae621724d1f06c070c2fc0f4a27bb3719d4053c2
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 17:39:58 +0200

    Add first test case

--- src/main/java/com/example/unittests/task3/ShoppingCart.java
+++ src/main/java/com/example/unittests/task3/ShoppingCart.java
@@ -1,5 +1,50 @@
 package com.example.unittests.task3;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
 class ShoppingCart {
+
+    ShoppingCartList getShoppingCart() {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+    private static final Map<String, Product> PRODUCTS_DATABASE = Map.of(
+            "Iceberg",
+            new Product(
+                    "Iceberg",
+                    BigDecimal.valueOf(1.55),
+                    BigDecimal.valueOf(15.0),
+                    BigDecimal.valueOf(21.0)
+            ),
+            "Tomato",
+            new Product(
+                    "Tomato",
+                    BigDecimal.valueOf(0.52),
+                    BigDecimal.valueOf(15.0),
+                    BigDecimal.valueOf(21.0)
+            ),
+            "Chicken",
+            new Product(
+                    "Chicken",
+                    BigDecimal.valueOf(1.34),
+                    BigDecimal.valueOf(12.0),
+                    BigDecimal.valueOf(21.0)
+            ),
+            "Bread",
+            new Product(
+                    "Bread",
+                    BigDecimal.valueOf(0.71),
+                    BigDecimal.valueOf(12.0),
+                    BigDecimal.valueOf(10.0)
+            ),
+            "Corn",
+            new Product(
+                    "Corn",
+                    BigDecimal.valueOf(1.21),
+                    BigDecimal.valueOf(12.0),
+                    BigDecimal.valueOf(10.0)
+            )
+    );
 
 }
--- src/test/java/com/example/unittests/task3/ShoppingCartTests.java
+++ src/test/java/com/example/unittests/task3/ShoppingCartTests.java
@@ -1,5 +1,27 @@
 package com.example.unittests.task3;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.math.BigDecimal.ZERO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class ShoppingCartTests {
+
+    private final ShoppingCart shoppingCart = new ShoppingCart();
+
+    @Test
+    public void shouldListEmptyShoppingCart() {
+        // given/when
+        ShoppingCartList result = shoppingCart.getShoppingCart();
+
+        // then
+        assertEquals(List.of(), result.items());
+        assertNull(result.promotion());
+        assertEquals(0, result.totalProducts());
+        assertEquals(ZERO, result.totalPrice());
+    }
 
 }
