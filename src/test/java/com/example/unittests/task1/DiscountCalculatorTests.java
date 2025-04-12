931899974c79ed185b7ea848a8eb5e36e5bb910d
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 14:41:04 +0200

    Add test cases

--- src/test/java/com/example/unittests/task1/DiscountCalculatorTests.java
+++ src/test/java/com/example/unittests/task1/DiscountCalculatorTests.java
@@ -1,5 +1,37 @@
 package com.example.unittests.task1;
 
+import org.junit.jupiter.api.Test;
+
 class DiscountCalculatorTests {
+
+    @Test
+    public void shouldThrowExceptionWhenAmountIsNull() {
+
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenAmountIsLessThanZero() {
+
+    }
+
+    @Test
+    public void shouldNotApplyDiscountWhenAmountIsZero() {
+
+    }
+
+    @Test
+    public void shouldNotApplyDiscountWhenAmountIsTooLow() {
+
+    }
+
+    @Test
+    public void shouldApplyStandardDiscountWhenAmountIsBetweenStandardAndHighDiscountLevels() {
+
+    }
+
+    @Test
+    public void shouldApplyHighDiscountWhenAmountIsAboveHighDiscountLevel() {
+
+    }
 
 }
