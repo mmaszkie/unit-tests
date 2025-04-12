24ed8f31a38501aa5bb594b40dd9542109282f0e
Mateusz Maszkiewicz <mateusz.maszkiewicz@allegro.com>
2025-04-12 17:48:21 +0200

    Add second test case

--- src/test/java/com/example/unittests/task3/ShoppingCartTests.java
+++ src/test/java/com/example/unittests/task3/ShoppingCartTests.java
@@ -1,16 +1,25 @@
 package com.example.unittests.task3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static java.math.BigDecimal.ZERO;
+import static java.math.RoundingMode.UNNECESSARY;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ShoppingCartTests {
 
-    private final ShoppingCart shoppingCart = new ShoppingCart();
+    private ShoppingCart shoppingCart;
+
+    @BeforeEach
+    public void beforeEach() {
+        shoppingCart = new ShoppingCart();
+    }
 
     @Test
     public void shouldListEmptyShoppingCart() {
@@ -22,6 +31,40 @@ class ShoppingCartTests {
         assertNull(result.promotion());
         assertEquals(0, result.totalProducts());
         assertEquals(ZERO, result.totalPrice());
+    }
+
+    @Test
+    public void shouldAddProductToShoppingCart() {
+        // given
+        List<Item> expectedItems = List.of(
+                new Item("Iceberg", newBigDecimal(2.17), 1),
+                new Item("Tomato", newBigDecimal(0.73), 1),
+                new Item("Chicken", newBigDecimal(1.83), 1),
+                new Item("Bread", newBigDecimal(0.88), 1),
+                new Item("Corn", newBigDecimal(1.50), 1)
+        );
+
+        // when
+        shoppingCart.addProduct("Iceberg");
+        shoppingCart.addProduct("Tomato");
+        shoppingCart.addProduct("Chicken");
+        shoppingCart.addProduct("Bread");
+        shoppingCart.addProduct("Corn");
+
+        // then
+        ShoppingCartList result = shoppingCart.getShoppingCart();
+        List<Item> actualItems = result.items();
+
+        assertEquals(expectedItems.size(), actualItems.size());
+        assertTrue(expectedItems.containsAll(actualItems));
+
+        assertNull(result.promotion());
+        assertEquals(5, result.totalProducts());
+        assertEquals(newBigDecimal(7.11), result.totalPrice());
+    }
+
+    private BigDecimal newBigDecimal(double value) {
+        return BigDecimal.valueOf(value).setScale(2, UNNECESSARY);
     }
 
 }
